7b3d4364446870c4eb9bdaadd02944829de11783
cutiko <cutiko@gmail.com>
2019-06-24 10:25:28 -0400

    ADD: passing data from screen A to B

--- Songs.tsx
+++ Songs.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import * as React from 'react';
 import {Text} from 'react-native';
 import {NavigationScreenProps} from 'react-navigation';
 
-interface SongsProps extends NavigationScreenProps {}
+interface SongsProps extends NavigationScreenProps {
+  navigation : any
+  title: String
+}
 
-export default function (props : SongsProps) {
+const Songs : React.FunctionComponent<SongsProps> = (props : SongsProps)=> {
+    console.warn(props.navigation.state.params.title)
 
   return <Text style={{flex: 1, backgroundColor: 'green'}}/>;
 }
+
+export default Songs;
