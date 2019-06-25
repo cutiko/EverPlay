6a716d0e4c0a8d0bb6d45a859c23a5318638a85c
cutiko <cutiko@gmail.com>
2019-06-25 16:35:51 -0400

    ADD: songs to the reducer

--- reducers/combineReducers.ts
+++ reducers/combineReducers.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import playlistRowsReducer from "./playlistRowsReducers";
 import playlistReducer from "./playlistReducer";
+import songsReducer from "./songsReducer";
 
 export default combineReducers({
   playlistRows: playlistRowsReducer,
-  playlists: playlistReducer
+  playlists: playlistReducer,
+  songs: songsReducer,
 })
