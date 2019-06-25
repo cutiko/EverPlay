93a36f29d4433c91040acce50f3f9069e716c0f7
cutiko <cutiko@gmail.com>
2019-06-25 15:03:05 -0400

    ADD: data of specific playlist is obtained

--- containers/PlayList.tsx
+++ containers/PlayList.tsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import {aliasesNavigation, EverProps} from "../types/EverProps";
 import {PlayListRow} from "../models/PlayListRow";
 import styles from "../theme/styles";
 import {white} from "../theme/colors";
+import {Playlist, PlaylistState} from "../models/Playlist";
+import {connect} from "react-redux";
+import {playlists} from "../seed";
 
-class PlayList extends Component<EverProps> {
+interface PlayListProps extends EverProps{
+  playlists: PlaylistState
+}
+
+class PlayList extends Component<PlayListProps> {
 
   static navigationOptions = ({ navigation } : {navigation : aliasesNavigation}) => {
     const {name} : PlayListRow = navigation.getParam("playlistRow");
@@ -16,13 +23,33 @@ class PlayList extends Component<EverProps> {
 
   render() {
     const {key, name, count, color} = this.props.navigation.getParam("playlistRow");
-    const safeColor = color || white
-    return (
-      <View style={[styles.container, {backgroundColor: safeColor.toString()}]}>
-
-      </View>
-    )
+    return handleState(key, color, playlists)
   }
 }
 
-export default PlayList;
+function handleState(key: String, color: String, playlists: PlaylistState) {
+  if (key) {
+    const safeColor = color || white
+    const playlist : Playlist | undefined = playlists.get(key)
+    if (playlists) {
+      return (
+        <View style={[styles.container, {backgroundColor: safeColor.toString()}]}>
+
+        </View>
+      )
+    }
+  }
+  return (
+    <View style={[styles.container, {alignItems: 'center'}]}>
+      <Text>No playlist available</Text>
+    </View>
+  )
+}
+
+function mapStateToProps(state : any) {
+  return {
+    playlists: state.playlists
+  }
+}
+
+export default connect(mapStateToProps)(PlayList);
