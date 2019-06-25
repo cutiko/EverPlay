d885b4f8215c186a8d7148bce277f10418127820
cutiko <cutiko@gmail.com>
2019-06-25 16:19:01 -0400

    IMPROVE: constants are ment to be used

--- reducers/playlistReducer.ts
+++ reducers/playlistReducer.ts
@@ -1,12 +1,12 @@
 import {PlaylistState} from "../models/Playlist";
 import {playlists} from "../seed";
-import {PlaylistTypes} from "../actions/playlistAction";
+import {ADD_SONG, PlaylistTypes, REMOVE_SONG} from "../actions/playlistAction";
 
 export default function playlistReducer(state : PlaylistState = playlists, action : PlaylistTypes) {
   switch (action.type) {
-    case "ADD_SONG":
+    case ADD_SONG:
       return state
-    case "REMOVE_SONG":
+    case REMOVE_SONG:
       return state
     default:
       return state
