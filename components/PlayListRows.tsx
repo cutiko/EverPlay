2a7d08f6143d2846dd0168aac9cc18b6732bcf3f
cutiko <cutiko@gmail.com>
2019-06-25 11:51:52 -0400

    ADD: callback from flatlist to row

--- components/PlayListRows.tsx
+++ components/PlayListRows.tsx
@@ -4,13 +4,22 @@ import {PlayListRow, PlaylistRowsState} from "../models/PlayListRow";
 import PlaylistRowItem from "./PlaylistRowItem";
 
 interface RowsProps {
-  rows : PlaylistRowsState
+  rows: PlaylistRowsState
 }
 
 export default ({rows}: RowsProps) => {
-  const data : PlayListRow[] = Array.from(rows.values())
+  const data: PlayListRow[] = Array.from(rows.values())
 
-  const renderItem = ({item}: {item : PlayListRow})=> <PlaylistRowItem row={item}/>
+  const callback : any = (key: String) => {
+    console.warn(key)
+  }
+
+  const renderItem = ({item}: { item: PlayListRow }) => (
+    <PlaylistRowItem
+      row={item}
+      callback={callback}
+    />
+  )
 
   return (
     <FlatList data={data} renderItem={renderItem}/>
--- components/PlaylistRowItem.tsx
+++ components/PlaylistRowItem.tsx
@@ -5,13 +5,13 @@ import styles from "../theme/styles";
 import {white} from "../theme/colors";
 import row from '../theme/row'
 
-export default ({row: {name, count, color}}: { row: PlayListRow }) => {
+export default ({row: {name, count, color, key}, callback}: { row: PlayListRow, callback: any }) => {
   const safeColor = color || white
   return (
     <View style={row.container}>
       <View style={[{backgroundColor: safeColor.toString()}, row.tag]}/>
       <View style={row.wrapper}>
-        <Text style={[styles.textColor, row.text]}>{name}</Text>
+        <Text style={[styles.textColor, row.text]} onPress={()=>callback(key)}>{name}</Text>
         <Text>{count}</Text>
       </View>
     </View>
