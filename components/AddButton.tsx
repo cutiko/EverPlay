d1fa40d5c8d7b210db789556fd66d690bdd5f93a
cutiko <cutiko@gmail.com>
2019-06-25 16:08:35 -0400

    ADD: add button for header

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Text} from 'react-native'
 
-export default (props: any) => (
+export default ({callback} : {callback: any}) => (
   <Text style={{
     fontSize: 21,
     padding: 8,
     fontWeight: 'bold'
-  }}>
+  }}
+        onPress={()=>callback()}
+  >
     +
   </Text>
 )
