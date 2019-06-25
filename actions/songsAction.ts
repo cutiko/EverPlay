19c7bbd87b7bbe315a7081ef8a4345eb086b41ef
cutiko <cutiko@gmail.com>
2019-06-25 17:39:33 -0400

    ADD: handle state on reducer for songs

--- actions/songsAction.ts
+++ actions/songsAction.ts
@@ -1,14 +1,19 @@
 export const ADD_PLAYLIST = "ADD_PLAYLIST"
 export const REMOVE_PLAYLIST = "REMOVE_PLAYLIST"
 
+export interface SongPayload {
+  songId: String
+  playlistId: String
+}
+
 export interface addPlaylistAction {
   type: typeof ADD_PLAYLIST,
-  payload: String
+  payload: SongPayload
 }
 
 export interface removePlaylistAction {
   type: typeof REMOVE_PLAYLIST
-  payload: String
+  payload: SongPayload
 }
 
 export type SongsTypes = addPlaylistAction | removePlaylistAction
--- reducers/songsReducer.ts
+++ reducers/songsReducer.ts
@@ -5,10 +5,25 @@ import {songs} from "../seed";
 export default function songsReducer(state : SongState = songs, action : SongsTypes) {
   switch (action.type) {
     case ADD_PLAYLIST:
-      return state
+      return changeSongPlaylist(state, action.payload.songId, action.payload.playlistId, true)
     case REMOVE_PLAYLIST:
-      return state
+      return changeSongPlaylist(state, action.payload.songId, action.payload.playlistId, false)
     default:
       return state
   }
+}
+
+function changeSongPlaylist(state: SongState, songId: String, playlistId: String, remove : boolean) {
+  const copy = {...state}
+  const song = copy.get(songId)
+  if (song) {
+    if (remove) {
+      song.playlists.delete(playlistId)
+    } else {
+      song.playlists.set(playlistId, true)
+    }
+    return copy
+  }
+  return state
+
 }
