9f55e6e5deb2a0b3c92c64bbf89fc14f1864d8e2
cutiko <cutiko@gmail.com>
2019-06-25 14:03:15 -0400

    REFACTOR: add explicit type to reducer

--- reducers/playlistRowsReducers.ts
+++ reducers/playlistRowsReducers.ts
@@ -2,9 +2,7 @@ import {playlistsRows} from "../seed";
 import {PlaylistRowsState} from "../models/PlayListRow";
 import {PlayListRowTypes} from "../actions/playlistRowAction";
 
-const initialState = playlistsRows
-
 //TODO switch case for action to update the rows using the update row action
-export default function playlistRowsReducer(state = initialState, action : PlayListRowTypes) : PlaylistRowsState {
+export default function playlistRowsReducer(state : PlaylistRowsState = playlistsRows, action : PlayListRowTypes) : PlaylistRowsState {
   return state
 }
