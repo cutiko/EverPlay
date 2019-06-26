54e61ac1fe76cf3800ad397ae07cbc09f9fff43f
cutiko <cutiko@gmail.com>
2019-06-26 13:06:40 -0400

    IMPROVE: one capital case String missed

--- components/FlatSongs.tsx
+++ components/FlatSongs.tsx
@@ -3,7 +3,7 @@ import {FlatList} from "react-native";
 import {Song, SongState} from "../models/Song";
 import ItemSong from './ItemSong'
 
-export default ({songs, listId, callback} : {songs: SongState, listId: String, callback : Function})=> {
+export default ({songs, listId, callback} : {songs: SongState, listId: string, callback : Function})=> {
   const data = Array.from(songs.values())
 
   const renderItem = ({item} : {item : Song})=> (
